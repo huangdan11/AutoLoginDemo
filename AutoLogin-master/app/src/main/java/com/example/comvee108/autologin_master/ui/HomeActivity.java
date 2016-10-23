9e00e6b6627e587e166ae7dd57f20d54b5d47a03
lb1207087645 <1207087645@qq.com>
2016-10-23 22:23:05 +0800

    send commit
    正式项目

---
+++ AutoLogin-master/app/src/main/java/com/example/comvee108/autologin_master/ui/HomeActivity.java
@@ -0,0 +1,18 @@
+package com.example.comvee108.autologin_master.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.comvee108.autologin_master.R;
+
+/**
+ * 主页界面
+ */
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ AutoLogin-master/app/src/main/java/com/example/comvee108/autologin_master/ui/LoginActivity.java
@@ -0,0 +1,62 @@
+package com.example.comvee108.autologin_master.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.comvee108.autologin_master.R;
+import com.example.comvee108.autologin_master.manage.UserManage;
+
+/**
+ * 登录页面
+ * create by linbin
+ */
+public class LoginActivity extends Activity {
+    /**
+     * 用户名
+     */
+    private EditText edt_username;
+
+    /**
+     * 密码
+     */
+    private EditText edt_password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        initViews();
+    }
+
+    private void initViews() {
+        edt_username = (EditText) findViewById(R.id.edt_username);
+        edt_password = (EditText) findViewById(R.id.edt_password);
+        findViewById(R.id.btn_login).setOnClickListener(mOnClickListener);
+    }
+
+
+    private View.OnClickListener mOnClickListener = new View.OnClickListener() {
+
+
+        @Override
+        public void onClick(View view) {
+
+            switch (view.getId()) {
+                case R.id.btn_login://登录
+                    String userName = edt_username.getText().toString();
+                    String userPwd = edt_password.getText().toString();
+                    UserManage.getInstance().saveUserInfo(LoginActivity.this, userName, userPwd);
+                    Toast.makeText(LoginActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);//跳转到主页
+                    startActivity(intent);
+                    finish();
+                    break;
+            }
+
+        }
+    };
+}
